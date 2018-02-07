11a2824e6e878eb6553598b462015cd75d5205f5
Sze Tyng Lee <lee.szetyng@gmail.com>
2018-02-07 15:33:23 +0000

    Changed from sleep to looping to connect to the internet

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
+# to load onto ESP8266
+
 from machine import Pin, I2C
 import json
 import network
 import time
 
 import machine
-import ubinascii
 from umqtt.simple import MQTTClient
-#clientID = ubinascii.hexlify(machine.unique_id())
-
 
 # initialising ESP 
 i2c = I2C(sda=Pin(4), scl=Pin(5), freq=100000)
-# i2c.writeto_mem(0x13, 0x80, bytearray([0x08]))
-# data = i2c.readfrom_mem(0x13, 0x87, 2)
-# dataInInt = int.from_bytes(data, 'big')
-# print(dataInInt)
 
 # connect to broker
+network_name = 'EEERover'
+network_pw = 'exhibition'
 ap_if = network.WLAN(network.AP_IF)
 ap_if.active(False)
-time.sleep_ms(1000)
 sta_if = network.WLAN(network.STA_IF)
-time.sleep_ms(1000)
-sta_if.active(True)
-time.sleep_ms(1000)
-sta_if.connect('EEERover', 'exhibition')
-time.sleep_ms(2000)
-if sta_if.isconnected(): 
-	print('connecteddd :D')
-	print('config set:', sta_if.ifconfig())
-else:
-	print('nope :(')
+if not sta_if.isconnected():
+    print('connecting to network...')
+    sta_if.active(True)
+    sta_if.connect(network_name, network_pw)
+    while not sta_if.isconnected():
+        pass
+print('network config:', sta_if.ifconfig())
+
+
+sta_if = network.WLAN(network.STA_IF)
+while not sta_if.isconnected():
+	sta_if.active(True)
+	sta_if.connect(network_name, network_pw)
+print('connected to the network')
 
 # connect to broker
-client = MQTTClient('14140928', '192.168.0.10')
+broker_id = '192.168.0.10'
+client = MQTTClient(machine.unique_id(), broker_id)
 time.sleep_ms(1000)
 client.connect()
 
