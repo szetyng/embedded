d604909c0fc5e76cfd0f0831077c9b9ee6b566c1
Sze Tyng Lee <lee.szetyng@gmail.com>
2018-02-07 15:58:32 +0000

    Made doConnect function for readability
    Cannot do the same for connecting to broker, need access to client outside the scope of the function

--- main.py
+++ main.py
@@ -8,72 +8,45 @@ import time
 import machine
 from umqtt.simple import MQTTClient
 
-# initialising ESP 
-i2c = I2C(sda=Pin(4), scl=Pin(5), freq=100000)
+def doConnect():
+	network_name = 'EEERover'
+	network_pw = 'exhibition'
 
-# connect to broker
-network_name = 'EEERover'
-network_pw = 'exhibition'
-ap_if = network.WLAN(network.AP_IF)
-ap_if.active(False)
-sta_if = network.WLAN(network.STA_IF)
-if not sta_if.isconnected():
-    print('connecting to network...')
-    sta_if.active(True)
-    sta_if.connect(network_name, network_pw)
-    while not sta_if.isconnected():
-        pass
-print('network config:', sta_if.ifconfig())
+	ap_if = network.WLAN(network.AP_IF)
+	ap_if.active(False)
 
-
-sta_if = network.WLAN(network.STA_IF)
-while not sta_if.isconnected():
-	sta_if.active(True)
-	sta_if.connect(network_name, network_pw)
-print('connected to the network')
-
-# connect to broker
-broker_id = '192.168.0.10'
-client = MQTTClient(machine.unique_id(), broker_id)
-time.sleep_ms(1000)
-client.connect()
-
-# def doConnect():
-# 	ap_if = network.WLAN(network.AP_IF)
-# 	ap_if.active(False)
-# 	#time.sleep_ms(1000)
-# 	sta_if = network.WLAN(network.STA_IF)
-# 	#time.sleep_ms(1000)
-# 	sta_if.active(True)
-# 	time.sleep_ms(1000)
-# 	sta_if.connect('EEERover', 'exhibition')
-# 	#time.sleep_ms(2000)
-# 	if sta_if.isconnected(): 
-# 		print('connecteddd :D')
-# 		print('config set:', sta_if.ifconfig())
-# 	else:
-# 		print('nope :(')
+	sta_if = network.WLAN(network.STA_IF)
+	if not sta_if.isconnected():
+	    print('connecting to network...')
+	    sta_if.active(True)
+	    sta_if.connect(network_name, network_pw)
+	    while not sta_if.isconnected():
+	        pass
+	# print('network config:', sta_if.ifconfig())
 
 # read data and return it in JSON format
 def doReadings():
 	i2c.writeto_mem(0x13, 0x80, bytearray([0x08]))
 	data = i2c.readfrom_mem(0x13, 0x87, 2)
 	dataInInt = int.from_bytes(data, 'big')
-	print(dataInInt)
+	print('data in int:', dataInInt)
+	
 	read1json = {"proximity": dataInInt}
 	return json.dumps(read1json)
 
 def sendData():
-	# import machine
-	# import ubinascii
-	# from umqtt.simple import MQTTClient
-	#clientID = ubinascii.hexlify(machine.unique_id())
-
 	payload = doReadings()
-
-	# client = MQTTClient('14140928', '192.168.0.10')
-	# time.sleep_ms(1000)
-	# client.connect()
-	#time.sleep_ms(1000)
 	client.publish('esys/<anonymous>' ,bytes(payload,'utf-8'))
-	
+
+
+# initialising ESP 
+i2c = I2C(sda=Pin(4), scl=Pin(5), freq=100000)
+
+# connect to internet
+doConnect()
+
+# connect to broker
+broker_id = '192.168.0.10'
+client = MQTTClient(machine.unique_id(), broker_id)
+#time.sleep_ms(1000)
+client.connect()
