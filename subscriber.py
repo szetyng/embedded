e6de91d4b3304d15b937cfbea9e627f590cd3294
moh1ka <mg5215@ic.ac.uk>
2018-02-14 15:57:38 +0000

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -51,7 +51,7 @@ def get_ranking(score):
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-	# Subscribing in on_connect() means that if we lose the connection and
+    # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("esys/anonymous")
 
